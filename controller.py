afac96905af48a491d05bb2191ab6e15b9b85698
Carsten Thue-Bludworth <carstentb@protonmail.com>
2022-07-31 00:03:31 -0400

    Got some flow entry stuff working. Updated mn.py script to use autoSetMacs

--- controller.py
+++ controller.py
@@ -2,10 +2,12 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_0
 # from ryu.lib.mac import haddr_to_bin
-# from ryu.lib.packet import packet
+from ryu.lib.packet import packet, ethernet, arp, ipv4, ipv6, icmp
 from ryu.topology import event
+
+import time
 
 # from unigui.pygamedisplay import PygameDisplay
 # from unigui.colorscheme import VSCode
@@ -16,7 +18,7 @@ class Controller(app_manager.RyuApp):
     An RYU OpenFlow controller that knows the global network topology and can route
     packets in order to load-balance the network.
     """
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         """
@@ -25,8 +27,12 @@ class Controller(app_manager.RyuApp):
         super(Controller, self).__init__(*args, **kwargs)
         # TODO
         self.running = True
-        # We'll keep a list of the switches that are connected here
+        # We'll keep a running list of hosts and switches that are connected here
         self.switches = []
+        # Keep a dictionary of hosts, mapping MAC address to the (switch_idx, port) tuple it's connected to
+        self.hosts = {}
+        # Kepp an ARP table, mapping MAC addresses to IP addresses
+        self.arp_table = {}
         # self.gui = TextWidget("tw", 0, 0, 500, 500, colorscheme=VSCode.dark)
         # self.gui.set_value("hello")
         # self.display = PygameDisplay(500, 500)
@@ -37,8 +43,11 @@ class Controller(app_manager.RyuApp):
         """
         Add a flow entry to a switch
         """
-        # TODO
-        return
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, actions=actions, hard_timeout=0, cookie=0, command=ofproto.OFPFC_ADD)
+        datapath.send_msg(mod)
 
     @set_ev_cls(event.EventSwitchEnter)
     def _switch_enter_handler(self, ev):
@@ -89,7 +98,58 @@ class Controller(app_manager.RyuApp):
         """
         Function called when an OpenFlow packet is received by the controller.
         """
+        # First get the relevant data about the packet:
         msg = ev.msg
         datapath = msg.datapath
+        switch_idx = -1
+        if datapath in self.switches:
+            switch_idx = self.switches.index(datapath)
+        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        print("Got an OFPPacketIn message: {}".format(msg))
+        in_port = msg.in_port
+        pkt = packet.Packet(msg.data)
+        
+        # Debug printing:
+        print("\n[" + time.ctime() + "]: Got an OFPPacketIn message: {}".format(msg))
+        print("    in_port is {}".format(in_port))
+        print("    Packet contains the following protocols: ", end='')
+        for p in pkt.protocols:
+            print(p.protocol_name + ' ', end='')
+        print('')
+        print("    pkt is {}".format(pkt), end='\n\n')
+
+        # Get the Ethernet header info
+        eth = pkt.get_protocol(ethernet.ethernet)
+        src = eth.src
+        dst = eth.dst
+
+        # Regardless of protocol type, add the host to our list
+        if src not in self.hosts:
+            self.hosts[src] = (switch_idx, in_port)
+        print("self.hosts is {}".format(self.hosts))
+
+
+        # For IPv6 packets, just drop the packet (by having an empty actions list)
+        if pkt.get_protocol(ipv6.ipv6):
+            print("Adding an IPv6 flow entry!")
+            match = parser.OFPMatch(dl_type=eth.ethertype)
+            actions = []
+            self.add_flow(datapath, 0, match, actions)
+
+        # For ARP packets
+        _arp = pkt.get_protocol(arp.arp)
+        if _arp:
+            src_ip = _arp.src_ip
+            dst_ip = _arp.dst_ip
+            print("Adding an ARP flow entry!")
+            match = parser.OFPMatch(dl_type=eth.ethertype)
+            actions = [parser.OFPActionOutput(ofproto.OFPP_FLOOD)]
+            self.add_flow(datapath, 0, match, actions)
+
+        # For IPv4 Packets, do something else
+        _ipv4 = pkt.get_protocol(ipv4.ipv4)
+        if _ipv4:
+            print("Adding an IPv4 flow entry!")
+            match = parser.OFPMatch(dl_type=eth.ethertype)
+            actions = [parser.OFPActionOutput(ofproto.OFPP_NORMAL)]  # TODO Fix the flood here
+            self.add_flow(datapath, 0, match, actions)
--- mn.py
+++ mn.py
@@ -14,33 +14,33 @@ from mininet.node import RemoteController
 class StarTopo(Topo):
     "Simple topology example."
 
-    def __init__( self ):
+    def __init__(self):
         "Create custom topo."
 
         # Initialize topology
-        Topo.__init__( self )
+        Topo.__init__(self)
 
         # Add hosts and switches
         hosts = [
-            self.addHost('h0', ip='10.0.0.2/24'),
-            self.addHost('h1', ip='10.0.0.3/24'),
-            self.addHost('h2', ip='10.0.0.4/24'),
+            self.addHost('h1', ip='10.0.0.1/24'),
+            self.addHost('h2', ip='10.0.0.2/24'),
+            self.addHost('h3', ip='10.0.0.3/24'),
         ]
         # server.cmd('bash h2-server-setup.sh')
         switches = [
             self.addSwitch('s0'),
-            self.addSwitch('s1'),
+            # self.addSwitch('s1'),
         ]
         
         # Add links
         for host in hosts:
             self.addLink(host, switches[0])
         # for switch in switches:
-        self.addLink(switches[0], switches[1])
+        # self.addLink(switches[0], switches[1])
 
 def network_test():
     topo = StarTopo()
-    net = Mininet(topo, controller=None)
+    net = Mininet(topo, controller=None, autoSetMacs=True)
     print("Connecting to controller...")
     net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
     print("Starting Mininet...")
