41cb76352f12caf02db4f7b05e8d8238f2f5fc30
Carsten Thue-Bludworth <carstentb@protonmail.com>
2022-08-01 13:19:33 -0400

    Got more data, updated plots

--- plot.py
+++ plot.py
@@ -3,15 +3,15 @@ import numpy as np
 
 # Plot for UDP Throughput
 fig, ax = plt.subplots()
-ax.set_ylim((0, 4.0))
+ax.set_ylim((0, 5.0))
 ax.set_ylabel('Throughput (Gbps)')
 ax.set_xlabel('Number of hops through network')
 ax.set_title('Simulated Throughput of UDP Packets')
 # Data
-x_labels = ['1', '2', '3']
+x_labels = ['1', '2', '3', '4']
 throughput = [
-    [3.01, 2.71, 2.43], # flow-enabled
-    [2.30, 2.27, 2.99], # flow-disabled
+    [3.80, 3.04, 2.64, 2.37], # flow-enabled
+    [2.44, 2.31, 2.22, 2.39], # flow-disabled
 ]
 x = np.arange(len(x_labels))
 ax.set_xticks(x, x_labels)
@@ -21,77 +21,56 @@ rects2 = ax.bar(x+bar_width/2, throughput[1], width=bar_width, color='blue',   l
 ax.legend()
 ax.bar_label(rects1, padding=3)
 ax.bar_label(rects2, padding=3)
-plt.savefig('plots/UDP_throughput.png')
+plt.savefig('plots_laptop/UDP_throughput.png')
 
 # Plot for TCP Throughput
 fig, ax = plt.subplots()
-ax.set_ylim((0, 42.0))
+ax.set_ylim((0, 50.0))
 ax.set_ylabel('Throughput (Gbps)')
 ax.set_xlabel('Number of hops through network')
 ax.set_title('Simulated Throughput of TCP Packets with Flow Entries')
 # Data
-x_labels = ['1', '2', '3']
+x_labels = ['1', '2', '3', '4']
 throughput = [
-    [35.2, 35.8, 32.4]
+    [45.84, 41.84, 40.64, 39.56]
 ]
 x = np.arange(len(x_labels))
 ax.set_xticks(x, x_labels)
 bar_width = 0.35
 rects1 = ax.bar(x, throughput[0], width=bar_width, color='orange', label='flow-enabled')
 ax.bar_label(rects1, padding=3)
-plt.savefig('plots/TCP_throughput_flow_enabled.png')
+plt.savefig('plots_laptop/TCP_throughput_flow_enabled.png')
 
 # Plot for TCP Throughput
 fig, ax = plt.subplots()
-ax.set_ylim((0, 20.0))
+ax.set_ylim((0, 8.0))
 ax.set_ylabel('Throughput (Mbps)')
 ax.set_xlabel('Number of hops through network')
 ax.set_title('Simulated Throughput of TCP Packets without Flow Entries')
 # Data
-x_labels = ['1', '2', '3']
+x_labels = ['1', '2', '3', '4']
 throughput = [
-    [15.1, 7.9, 5.4]
+    [6.57, 3.13, 2.19, 1.59]
 ]
 x = np.arange(len(x_labels))
 ax.set_xticks(x, x_labels)
 bar_width = 0.35
 rects1 = ax.bar(x, throughput[0], width=bar_width, color='orange')
 ax.bar_label(rects1, padding=3)
-plt.savefig('plots/TCP_throughput_flow_disabled.png')
+plt.savefig('plots_laptop/TCP_throughput_flow_disabled.png')
 
 
 # Plot for ICMP round-trip latency in flow-enabled controller
 fig, ax = plt.subplots()
-ax.set_ylim((0, 60))
-ax.set_ylabel('Latency/Jitter (microseconds)')
+ax.set_ylim((0, 0.40))
+ax.set_ylabel('Latency/Jitter (milliseconds)')
 ax.set_xlabel('Number of hops through network')
 ax.set_title('Round-trip Latency of ICMP Echo Requests with Flows Entries')
 # Data
-x_labels = ['1', '2', '3']
+x_labels = ['1', '2', '3', '4']
 throughput = [
-    [40, 43, 47],
-    [14, 9, 7]
-]
-x = np.arange(len(x_labels))
-ax.set_xticks(x, x_labels)
-rects1 = ax.bar(x-bar_width/2, throughput[0], width=bar_width, color='black', label='latency')
-rects2 = ax.bar(x+bar_width/2, throughput[1], width=bar_width, color='grey', label='jitter')
-ax.legend()
-ax.bar_label(rects1, padding=3)
-ax.bar_label(rects2, padding=3)
-plt.savefig('plots/ICMP_latency_flow_enabled.png')
-
-# Plot for ICMP round-trip latency in flow-enabled controller
-fig, ax = plt.subplots()
-ax.set_ylim((0, 6))
-ax.set_ylabel('Latency/Jitter (milliseconds)')
-ax.set_xlabel('Number of hops through network')
-ax.set_title('Round-trip Latency of ICMP Echo Requests without Flow Entries')
-# Data
-x_labels = ['1', '2', '3']
-throughput = [
-    [1.96, 3.54, 4.82],
-    [0.164, 0.445, 0.454]
+    [0.116, 0.176, 0.236, 0.240],
+    [0.059, 0.177, 0.339, 0.336]
 ]
 x = np.arange(len(x_labels))
 ax.set_xticks(x, x_labels)
@@ -100,4 +79,25 @@ rects2 = ax.bar(x+bar_width/2, throughput[1], width=bar_width, color='grey', lab
 ax.legend(loc='upper left')
 ax.bar_label(rects1, padding=3)
 ax.bar_label(rects2, padding=3)
-plt.savefig('plots/ICMP_latency_flow_disabled.png')
+plt.savefig('plots_laptop/ICMP_latency_flow_enabled.png')
+
+# Plot for ICMP round-trip latency in flow-disabled controller
+fig, ax = plt.subplots()
+ax.set_ylim((0, 36))
+ax.set_ylabel('Latency/Jitter (milliseconds)')
+ax.set_xlabel('Number of hops through network')
+ax.set_title('Round-trip Latency of ICMP Echo Requests without Flow Entries')
+# Data
+x_labels = ['1', '2', '3', '4']
+throughput = [
+    [7.42, 13.19, 19.08, 29.20],
+    [2.09, 4.76, 7.27, 12.21]
+]
+x = np.arange(len(x_labels))
+ax.set_xticks(x, x_labels)
+rects1 = ax.bar(x-bar_width/2, throughput[0], width=bar_width, color='black', label='latency')
+rects2 = ax.bar(x+bar_width/2, throughput[1], width=bar_width, color='grey', label='jitter')
+ax.legend(loc='upper left')
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+plt.savefig('plots_laptop/ICMP_latency_flow_disabled.png')
